43d9d36dd6faa4bf227bae6d660cca8a2e71e20d
DaniilBatiuk <sportak50@gmail.com>
2023-05-10 13:11:34 +0300

    Добавьте файлы проекта.

---
+++ Azure_Dz_5/Functions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_Dz_5
+{
+    public static class Functions
+    {
+        public static void ProcessBlobData(
+    [BlobTrigger("images/{filename}")] Stream inputFile,
+    string filename,
+    [Blob("images-min/copy-{filename}", FileAccess.Write)] Stream outputFile,
+    ILogger logger
+    )
+        {
+            logger.LogInformation($"C# Blob trigger function processed blob\n Name:{filename}");
+
+            // Load image from stream
+            var image = Image.Load(inputFile);
+
+            // Resize image
+            image.Mutate(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(200, 100),
+                Mode = ResizeMode.Max
+            }));
+
+            // Save resized image to stream
+            image.Save(outputFile, new JpegEncoder());
+
+            logger.LogInformation($"Successfully resized and saved image {filename}");
+
+        }
+    }
+
+}
---
+++ Azure_Dz_5/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostBuilder builder = new HostBuilder();
+builder.ConfigureWebJobs(options => {
+    options.AddAzureStorageBlobs();
+    options.AddAzureStorageQueues();
+});
+builder.ConfigureLogging((HostBuilderContext context, ILoggingBuilder logBuilder) => {
+    logBuilder.AddConsole();
+});
+IHost host = builder.Build();
+using (host)
+    host.Run();
